a6584bf8a999d2d87a253b39bd06723b38436324
Anamika777 <114104159+Anamika777@users.noreply.github.com>
2023-01-19 15:28:06 +0530

    new
    code is changed

--- src/test/java/practice_package/CreateTest.java
+++ src/test/java/practice_package/CreateTest.java
@@ -5,8 +5,9 @@ public class CreateTest {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("India----------");
+		System.out.println("england----------");
 System.out.println("BHARAT----------------");
+
 	}
 
 }
