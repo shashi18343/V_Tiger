58a875fb829e0cedd8ddb8118e52570f438738f2
Admin <Admin@DESKTOP-93AUPV9>
2023-01-19 12:28:04 +0530

    commit4

--- src/test/java/practice_package/SamplePP1Test.java
+++ src/test/java/practice_package/SamplePP1Test.java
@@ -5,6 +5,9 @@ public class SamplePP1Test {
 	public static void main(String[] args) {
 
 		System.out.println("hello");
+		
+		System.out.println("goood morning lead");
+		System.out.println("good evening lead");
 	}
 
 }
