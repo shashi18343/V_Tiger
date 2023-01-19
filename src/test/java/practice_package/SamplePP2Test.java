9bfe3c9f2bfed3602c1d315588c853ee02a39fb9
Admin <Admin@DESKTOP-93AUPV9>
2023-01-19 12:58:35 +0530

    city commit

--- src/test/java/practice_package/SamplePP2Test.java
+++ src/test/java/practice_package/SamplePP2Test.java
@@ -5,6 +5,9 @@ public class SamplePP2Test {
 	public static void main(String[] args) {
 
 		System.out.println("world");
+		System.out.println("new york");
+		System.out.println("delhi");
+		System.out.println("kanpur");
 	}
 
 }
